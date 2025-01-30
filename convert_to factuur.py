d1e11d43b6a0f074b527cca28fd13318dc90be2f
cholwick <170406890+cholwick@users.noreply.github.com>
2025-01-30 15:11:58 +0100

    json converter

---
+++ convert_to factuur.py
@@ -0,0 +1,46 @@
+#open order .json file
+# with open('2000-096.json') as file:
+# inlezen data uit verhaal
+#toevoegen bedrijsgegevens aan data
+#openen factuur.jsonfile
+#schrijf data naar json weg
+
+import json
+
+with open('2000-096.json') as file:
+    data = json.load(file)
+
+#inklosief btw bedrag berekenen per order regel
+#totaal bedreg van factuur berekenen
+#kijken hoe om het toe te voegen aan de factuur
+
+producten = data['order']['producten']
+totaal = 0
+for product in producten:
+    print(product)
+    prijs_zon_btw = product['aantal']*product['prijs_per_stuk_excl_btw']
+    btw = (product['btw_percentage'] /100 ) * prijs_zon_btw
+    product['btw']= round(btw,2)
+    totaal = round(prijs_zon_btw + btw,2)
+print(totaal)
+
+# product['btw_bedrag'] = product['aantal'] * product['prijs'] * product['btw'] / 100
+# product['totaal'] = product['aantal'] * product['prijs'] + product['btw_bedrag']
+
+totalen = {"totaal_incld_btw": totaal}
+bedrijfsgegevens = {"bedrijfsgegevens":
+  {"bedrijfsnaam": 'Soft log',
+    "adres": '1234 CK,Rottendam',
+    "kvknummer": '123456789',
+    "btwnummer": 'NL 12345678901',
+    "telefoonnummer": '+088 98 73 87 32',
+    "email": 'info@softlog.nl',}
+    
+  }
+
+data['bedrijfsgegevens']= bedrijfsgegevens
+data['totalen'] = totalen
+out_file = open("myfile.json", "w")
+
+y = json.dump(data, out_file, indent=4)
+
--- pdf_maker.py
+++ pdf_maker.py
@@ -1,4 +1,17 @@
 from fpdf import FPDF
+import json
+
+with open('2000-096.json') as file:#open the json file
+    data = json.load(file)
+
+ordernummer = data['order']['ordernummer']
+orderdatum = data['order']['orderdatum']
+betaaltermijn = data['order']['betaaltermijn']
+naam = data['order']['klant']['naam']
+adress= data['order']['klant']['adress']
+postcode = data['order']['klant']['postcode']
+stad = data['order']['klant']['stad']
+kvknummer = data['order']['klant']['KVK-nummer']
 
 # Define a class that extends FPDF to include the create_table method
 class PDF(FPDF):
@@ -37,21 +50,21 @@ pdf.set_font("Helvetica", "B", 24)
 
 # Add a cell (no need for ln=True anymore, instead use new_x and new_y)
 pdf.cell(140, 10, "factuur")
-pdf.cell(40, 10, "Soft log", ln=True)
+pdf.cell(40, 10, "Soft log", ln=1)
 
 pdf.set_font("Helvetica", "", 12)
-pdf.cell(25, 10, "Datum:",  border=True)
+pdf.cell(25, 10, "Datum:",  border=1)
 pdf.set_font("Helvetica", "B", 20)
 pdf.cell(115, 10, "factuurnummer:", border=False, align="C")
 pdf.set_font("Helvetica", "", 12)
-pdf.cell(0, 10, "+088 98 73 87 32",ln=True)
+pdf.cell(0, 10, "+088 98 73 87 32",ln=1)
 pdf.cell(90, 10, "12-12-2020")
-pdf.cell(50, 10, "f-0001")
-pdf.cell(10, 10, "info@softlog.nl",ln=True)
+pdf.cell(50, 10, data['ordernummer'], ln=1)
+pdf.cell(10, 10, "info@softlog.nl",ln=1)
 pdf.cell(140, 10, "Factuur aan:")
-pdf.cell(40, 10, "KVK nr: 12345678", ln=True)
+pdf.cell(40, 10, "KVK nr: 12345678", ln=1)
 pdf.cell(140, 10, "Klaas 6")
-pdf.cell(40, 10, "BTW id: NL 12345678901", ln=True)
+pdf.cell(40, 10, "BTW id: NL 12345678901", ln=1)
 pdf.cell(70, 10, "1234 CK, Rotterdam")
 
 # Insert a line break
